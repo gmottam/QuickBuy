7b8463fd082a53952a9d7c19d4c75b0dc97552de
Guilherme Martins <guim.martins@outlook.com>
2019-06-05 22:48:31 -0300

    Criação das Entidades

---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,28 @@
+﻿using QuickBuy.Dominio.Enumerados;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool IsBoleto
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool IsCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool IsDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
